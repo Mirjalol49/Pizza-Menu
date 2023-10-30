5ce63dcaf9a4bf1037097c0c760944135dfb3f81
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2023-10-29 22:50:07 -0700

    minor issue is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-const Header = () => {
+const header = () => {
   return (
     <header className="header">
       <h1>Pizza Menu 🍕</h1>
@@ -6,4 +6,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default header;
